4eea8aae4d6ca540738bf78982c0b0034acbe7ef
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-07-16 19:42:59 +0100

    added chek if prime function

--- Prime_Number_Checker.cpp
+++ Prime_Number_Checker.cpp
@@ -3,7 +3,20 @@
 
 using namespace std;
 
-int main(){
+int check_if_prime(){
+    int input;
+    cout << "Please input your number for checking: ";
+    cin >> input;
+
+    if(input == input % input && input % 1){
+        cout << "Your number is a prime number.";
+    } else {
+        cout << "Your number is not a prime number.";
+    }
+}
+
+int main(int argc, char const *argv[])
+{
     /* code */
     return 0;
 }
